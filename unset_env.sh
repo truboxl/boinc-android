1de66df6e2a721d2bfcdcfb1abcb4f43b166919b
Jia Yuan Lo <jylo06g@gmail.com>
2020-04-27 01:34:19 +0800

    Check only for non-prefixed "export"

--- unset_env.sh
+++ unset_env.sh
@@ -6,7 +6,7 @@ if [ ! -z "$OLDPATH" ]; then
     export PATH="$OLDPATH"
 fi
 
-grep 'export' set_env.sh | sed -e 's/    //g' | sed -e 's/grep.*//g' | sed -e 's/#export /export /g' | sed -e 's/export /unset /g' | sed -e 's/unset PATH.*//g' | sed -e 's/=.*//g' | sort | uniq | uniq -u > env1
+grep '^export' set_env.sh | sed -e 's/    //g' | sed -e 's/grep.*//g' | sed -e 's/export /unset /g' | sed -e 's/unset PATH.*//g' | sed -e 's/=.*//g' | sort | uniq | uniq -u > env1
 . ./env1
 
 # echo
