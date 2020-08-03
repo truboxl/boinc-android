6dfeae9c588559b2c84a8776367079473c15654e
Jia Yuan Lo <jylo06g@gmail.com>
2020-08-03 12:38:02 +0800

    Remove 80_optional_patches.txt
    Optional patches will be added back to the repo in a later date

--- 80_optional_patches.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Optional feature patches for BOINC
-
-if [ ! -d ./patch ]; then
-    git clone 'https://github.com/truboxl/boinc-feature-patch' ./patch || exit 1
-fi
-
-LINE_NO="$(grep 'Instructions' ./patch/README.md -n | sed -e 's/:### Instructions//')"
-tail -n "+$LINE_NO" ./patch/README.md
